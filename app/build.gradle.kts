3a8e911414c825d3900d9beca6cccb6dd2c61e2a
aleksandreev2 <sashahumortele3@gmail.com>
2026-07-22 00:28:30 +0400

    Bump DLR Gallery to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dlrgallery.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
