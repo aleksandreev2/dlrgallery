a7fcfd9cf37b696487f147e7c43e6708f7afc1ac
aleksandreev2 <sashahumortele3@gmail.com>
2026-07-21 22:54:59 +0400

    Apply settings before rendering the app

--- app/src/main/java/com/dlrgallery/app/MainActivity.kt
+++ app/src/main/java/com/dlrgallery/app/MainActivity.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.dlrgallery.app.ui.DLRGalleryApp
-import com.dlrgallery.app.ui.theme.DLRGalleryTheme
+import com.dlrgallery.app.ui.DLRGalleryRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DLRGalleryTheme {
-                DLRGalleryApp()
-            }
+            DLRGalleryRoot()
         }
     }
 }
